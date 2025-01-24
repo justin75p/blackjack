3e77d011982a6144144ce2def7a58bae3fe9131a
Justin <justin75p@gmail.com>
2025-01-23 22:16:28 -0800

    added cards and balance fields to Player

--- src/main/model/Player.java
+++ src/main/model/Player.java
@@ -1,5 +1,15 @@
 package main.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
+// Represents a player having cards and balance
 public class Player {
-    
+    private List<Card> cards;
+    private int balance;
+
+    public Player() {
+        cards = new ArrayList<Card>();
+        balance = 0;
+    }
 }
