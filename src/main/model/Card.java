34e548cee1640bcf8bd237a7eb18249c75b07815
Justin <justin75p@gmail.com>
2025-01-23 22:16:01 -0800

    added rank and suit enum as fields to Card

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -1,5 +1,11 @@
 package main.model;
 
+// Represents a Card, having a rank and a suit
 public class Card {
-    
+    private Rank rank;
+    private Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+
+    }
 }
