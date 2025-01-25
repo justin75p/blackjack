26cfc92b818b4a7875d868b9a10ec80e5f44c833
Justin <justin75p@gmail.com>
2025-01-24 23:45:36 -0800

    completed specification of CardHolder

--- src/main/model/CardHolder.java
+++ src/main/model/CardHolder.java
@@ -1,5 +1,30 @@
 package main.model;
 
-public abstract class CardHolder {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class CardHolder {
+    private List<Card> cards;
+
+    public CardHolder() {
+        cards = new ArrayList<>();
+    }
+
+    // EFFECTS: returns the total value of cards in hand
+    public int getHandValue() {
+        return 0;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: clears the holder's cards
+    public void resetHand() {
+        cards.clear();
+    }
+
+    // EFFECTS: checks if holder has a blackjack
+    public boolean hasBlackjack() {
+        return false;
+    }
+
+    public abstract void playTurn();
 }
