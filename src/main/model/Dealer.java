0849921282faab0358f481c083ff336e105a6932
Justin <justin75p@gmail.com>
2025-01-24 23:47:55 -0800

    begun the specification of dealer, making it extend CardHolder

--- src/main/model/Dealer.java
+++ src/main/model/Dealer.java
@@ -1,5 +1,18 @@
 package main.model;
 
-public class Dealer {
+public class Dealer extends CardHolder {
+
+    private Deck deck;
+
+    public Dealer() {
+        deck = new Deck();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: determines if the dealer should hit or stand
+    @Override
+    public void playTurn() {
+        
+    }
     
 }
