844952d1a62745b7d2ce05e665bee0fc4d1bac56
Justin <justin75p@gmail.com>
2025-02-07 22:07:19 -0800

    completed implementation of Dealer

--- src/main/model/Dealer.java
+++ src/main/model/Dealer.java
@@ -3,25 +3,39 @@ package main.model;
 public class Dealer extends CardHolder {
 
     private Deck deck;
+    private Card hiddenCard;
 
     public Dealer() {
         deck = new Deck();
         // Dealer has two cards, one hidden
         cards.add(deck.draw());
-        cards.add(deck.draw());
+        hiddenCard = deck.draw();
     }
 
     // MODIFIES: this
     // EFFECTS: determines if the dealer should hit or stand
     @Override
-    public void playTurn() {
-        
+    public boolean playTurn() {
+        // Dealer must hit
+        if (getHandValue() <= 16) {
+            cards.add(deck.draw());
+            return true;
+        } else { // Dealer must stand
+            return false;
+        }
     }
 
     // MODIFIES: this
     // EFFECTS: draws a card and deals it to player
     public void deal(Player player) {
+        Card card = deck.draw();
+        player.getCards().add(card);
+    }
 
+    // MODIFIES: this
+    // EFFECTS: shows the dealer's hidden card
+    public void showHiddenCard() {
+        cards.add(hiddenCard);
     }
     
 }
