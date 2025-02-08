1fefe15e10a0903856ff8afe298f464b5127b6e3
Justin <justin75p@gmail.com>
2025-02-07 22:41:54 -0800

    begun working on graphical interface

--- src/main/ui/BlackjackApp.java
+++ src/main/ui/BlackjackApp.java
@@ -1,7 +1,42 @@
 package main.ui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class BlackjackApp {
+    private JFrame frame;
+    private JPanel gamePanel;
+
     public BlackjackApp() {
-        
+        // Main frame
+        frame = new JFrame();
+        initializeMainFrame();
+
+        // Game panel
+        gamePanel = new JPanel();
+        initializeGamePanel();
+
+
+        frame.add(gamePanel);
+
+        frame.setVisible(true);
+    }
+
+    private void initializeMainFrame() {
+        frame.setTitle("Blackjack");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.pack();
+        frame.setSize(900, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+    }
+
+    private void initializeGamePanel() {
+        gamePanel.setLayout(new BorderLayout());
+        gamePanel.setBackground(new Color(53, 101, 77));
     }
 }
