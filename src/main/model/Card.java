0ae6c1ba8fd49b6a5b2b786e8638d61f91e5e4ca
Justin <justin75p@gmail.com>
2025-02-13 17:58:56 -0800

    Card now has a method getImageFileName which returns it's corresponding image file path

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -19,4 +19,38 @@ public class Card {
     public Suit getSuit() {
         return suit;
     }
+
+    // EFFECTS: returns the file image path of this card
+    public String getImageFileName() {
+        return "data/cards/" + getRankString() + "-" + getSuitString() + ".png";
+    }
+
+    private String getRankString() {
+        switch (rank) {
+            case ACE: return "A";
+            case TWO: return "2";
+            case THREE: return "3";
+            case FOUR: return "4";
+            case FIVE: return "5";
+            case SIX: return "6";
+            case SEVEN: return "7";
+            case EIGHT: return "8";
+            case NINE: return "9";
+            case TEN: return "10";
+            case JACK: return "J";
+            case KING: return "K";
+            case QUEEN: return "Q";
+            default: return "";
+        }
+    }
+
+    private String getSuitString() {
+        switch (suit) {
+            case CLUBS: return "C";
+            case DIAMONDS: return "D";
+            case HEARTS: return "H";
+            case SPADES: return "S";
+            default: return "";
+        }
+    }
 }
