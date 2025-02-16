be22cd47161c2a7a6ad7edb8ca52b0cbb4f3f02e
Justin <justin75p@gmail.com>
2025-02-16 13:54:53 -0800

    added a new boolean field to player indicating if their turn is over and related getter/setter

--- src/main/model/Player.java
+++ src/main/model/Player.java
@@ -3,12 +3,14 @@ package main.model;
 // Represents a player having cards and balance
 public class Player extends CardHolder {
     private final int INITIAL_BALANCE = 100;
+    private boolean turnOver;
     private int balance;
     private int amountWagered;
 
     public Player() {
         balance = INITIAL_BALANCE;
         amountWagered = 0;
+        turnOver = false;
     }
 
     // MODIFIES: this
@@ -30,5 +32,15 @@ public class Player extends CardHolder {
     // EFFECTS: returns player's balance
     public int getBalance() {
         return balance;
+    }
+
+    public boolean isTurnOver() {
+        return turnOver;
+    }
+
+    // MODIFIES: this
+    // sets the players turn to over when standing.
+    public void setTurnOver() {
+        turnOver = true;
     }
 }
