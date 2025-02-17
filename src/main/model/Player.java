67472999d7cf6166c1e93fd1d415e1fba1b45630
Justin <justin75p@gmail.com>
2025-02-16 16:24:41 -0800

    removed turnOver field and added a field to get the amount wagered

--- src/main/model/Player.java
+++ src/main/model/Player.java
@@ -3,14 +3,12 @@ package main.model;
 // Represents a player having cards and balance
 public class Player extends CardHolder {
     private final int INITIAL_BALANCE = 100;
-    private boolean turnOver;
     private double balance;
     private double amountWagered;
 
     public Player() {
         balance = INITIAL_BALANCE;
         amountWagered = 0;
-        turnOver = false;
     }
 
     // MODIFIES: this
@@ -24,6 +22,10 @@ public class Player extends CardHolder {
         return false;
     }
 
+    public double getAmountWagered() {
+        return amountWagered;
+    }
+
     // MODIFIES: this
     // EFFECTS: adds amount to player's balance
     public void addWinnings(double amount) {
@@ -34,15 +36,5 @@ public class Player extends CardHolder {
     // EFFECTS: returns player's balance
     public double getBalance() {
         return balance;
-    }
-
-    public boolean isTurnOver() {
-        return turnOver;
-    }
-
-    // MODIFIES: this
-    // sets the players turn to over when standing.
-    public void setTurnOver() {
-        turnOver = true;
     }
 }
