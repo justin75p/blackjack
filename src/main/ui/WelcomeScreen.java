eb603727a1274323b26d87440b76a5bffd7861e7
Justin <justin75p@gmail.com>
2025-08-09 15:38:43 -0700

    added a background image to the welcome screen

--- src/main/ui/WelcomeScreen.java
+++ src/main/ui/WelcomeScreen.java
@@ -1,9 +1,14 @@
 package main.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,13 +22,25 @@ public class WelcomeScreen {
     private JPanel buttonPanel;
     private JButton singleplayerButton;
     private JButton multiplayerButton;
-    
+
+    private static final String BACKGROUND_IMAGE_PATH = "data/backgrounds/welcome-screen-background.jpg";
 
     public WelcomeScreen() {
         frame = new JFrame();
         initializeMainFrame();
 
-        welcomePanel = new JPanel(new BorderLayout());
+        welcomePanel = new JPanel(new BorderLayout()) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                try {
+                    BufferedImage bgImage = ImageIO.read(new File(BACKGROUND_IMAGE_PATH));
+                    g.drawImage(bgImage, 0, 0, getWidth(), getHeight(), this);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
         initializeWelcomePanel();
 
         buttonPanel = new JPanel();
@@ -46,6 +63,9 @@ public class WelcomeScreen {
         titleLabel = new JLabel("BLACKJACK");
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
         titleLabel.setFont(new Font("Serif", Font.BOLD, 48));
+        titleLabel.setForeground(new Color(255, 255, 50));
+        titleLabel.setOpaque(true);
+        titleLabel.setBackground(new Color(0, 0, 0, 100));
 
         welcomePanel.add(titleLabel, BorderLayout.NORTH);
     }
@@ -55,6 +75,7 @@ public class WelcomeScreen {
         singleplayerButton.setFont(new Font("Serif", Font.BOLD, 18));
         singleplayerButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
         singleplayerButton.setMargin(new Insets(10, 50, 10, 50));
+
         multiplayerButton = new JButton("MULTIPLAYER");
         multiplayerButton.setFont(new Font("Serif", Font.BOLD, 18));
         multiplayerButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
