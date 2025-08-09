f2473e6cb2a0b8c8d726e118b9017e453fb0d156
Justin <justin75p@gmail.com>
2025-08-09 14:52:05 -0700

    basic welcome screen layout - visual polishes still needed

--- src/main/ui/WelcomeScreen.java
+++ src/main/ui/WelcomeScreen.java
@@ -1,22 +1,56 @@
 package main.ui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class WelcomeScreen {
     private JFrame frame;
+    private JLabel titleLabel;
+    private JPanel welcomePanel;
+    private JPanel buttonPanel;
     private JButton singleplayerButton;
     private JButton multiplayerButton;
     
 
     public WelcomeScreen() {
         frame = new JFrame();
+        initializeMainFrame();
+
+        welcomePanel = new JPanel(new BorderLayout());
+        initializeWelcomePanel();
+
+        buttonPanel = new JPanel();
+        initializeButtonPanel();
+
+        frame.add(welcomePanel);
+        frame.setVisible(true);
+    }
+
+    private void initializeMainFrame() {
         frame.setTitle("Blackjack");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setSize(800, 600);
+        frame.setSize(600, 500);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-        frame.setVisible(true);
+    }
+
+    private void initializeWelcomePanel() {
+        titleLabel = new JLabel("Blackjack");
+
+        welcomePanel.add(titleLabel, BorderLayout.NORTH);
+    }
+
+    private void initializeButtonPanel() {
+        singleplayerButton = new JButton("Singleplayer");
+        multiplayerButton = new JButton("Multiplayer");
+
+        buttonPanel.add(singleplayerButton);
+        buttonPanel.add(multiplayerButton);
+        welcomePanel.add(buttonPanel, BorderLayout.CENTER);
     }
 }
